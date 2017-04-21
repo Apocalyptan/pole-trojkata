c9de3626a908122446001e1d68e443730d0bd5ff
Adam Rudny <rudnyadam@gmail.com>
2017-04-21 14:27:38 +0200

    added multiplication

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
-var a = prompt("Wpisz podstawę trójkąta: ");
-	h = prompt("Wpisz wysokość trójkąta: ");
-	triangleArea = a*h/2;
+	var a = prompt("Wpisz podstawę trójkąta: ");
+		h = prompt("Wpisz wysokość trójkąta: ");
+		mnozOne = prompt("Cyfra do mnożenia I: ");
+		mnozTwo = prompt("Cyfra do mnożenia II: ");
+		triangleArea = a*h/2;
+		mnozWynik = mnozOne*mnozTwo;
 
-alert("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + "!");
-console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + "!");
+	console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + "!");
+	console.log("Wynik mnożenia to: " + mnozWynik);
+	alert("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + ", a wynik mnożenia wynosi: " + mnozWynik);
