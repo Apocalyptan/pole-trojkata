321e6d177eb630d1c454d13719cbdb3723b31683
Adam Rudny <rudnyadam@gmail.com>
2017-04-21 14:33:34 +0200

    Added name-fix

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-	var a = prompt("Wpisz podstawę trójkąta: ");
-		h = prompt("Wpisz wysokość trójkąta: ");
-		mnozOne = prompt("Cyfra do mnożenia I: ");
-		mnozTwo = prompt("Cyfra do mnożenia II: ");
-		triangleArea = a*h/2;
-		mnozWynik = mnozOne*mnozTwo;
+var a = prompt("Wpisz podstawę trójkąta: ");
+	h = prompt("Wpisz wysokość trójkąta: ");
+	mNumberOne = prompt("Cyfra do mnożenia I: ");
+	mNumberTwo = prompt("Cyfra do mnożenia II: ");
+	triangleArea = a*h/2;
+	multiplication = mNumberOne*mNumberTwo;
 
-	console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + "!");
-	console.log("Wynik mnożenia to: " + mnozWynik);
-	alert("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + ", a wynik mnożenia wynosi: " + mnozWynik);
+console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + "!");
+console.log("Wynik mnożenia to: " + multiplication);
+alert("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + ", a wynik mnożenia wynosi: " + multiplication);
