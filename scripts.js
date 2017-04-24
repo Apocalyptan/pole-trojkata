53b52e454a5427871665ed316f7b0cf7e93395bc
Adam Rudny <rudnyadam@gmail.com>
2017-04-24 11:02:06 +0200

    Change `;` for `,`

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-var a = prompt("Wpisz podstawę trójkąta: ");
-	h = prompt("Wpisz wysokość trójkąta: ");
-	mNumberOne = prompt("Cyfra do mnożenia I: ");
-	mNumberTwo = prompt("Cyfra do mnożenia II: ");
-	triangleArea = a*h/2;
+var a = prompt("Wpisz podstawę trójkąta: "),
+	h = prompt("Wpisz wysokość trójkąta: "),
+	mNumberOne = prompt("Cyfra do mnożenia I: "),
+	mNumberTwo = prompt("Cyfra do mnożenia II: "),
+	triangleArea = a*h/2,
 	multiplication = mNumberOne*mNumberTwo;
 
 console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + "!");
